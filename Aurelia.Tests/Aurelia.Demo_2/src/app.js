48349dab5dc73d319d18570b78a95b75b30dbace
potosik <potosik@gmail.com>
2016-06-20 16:11:18 +0300

    Demo_2 complete

--- Aurelia.Tests/Aurelia.Demo_2/src/app.js
+++ Aurelia.Tests/Aurelia.Demo_2/src/app.js
@@ -4,9 +4,19 @@ export class App {
 
         this.firstName = 'Artsiom';
         this.lastName = 'Patotski';
+
+        this.people = [];
     }
     
     get fullName() {
         return `${this.firstName} ${this.lastName}`;
+    }
+
+    create() {
+        this.people.push(this.fullName);
+    }
+
+    delete($index) {
+        this.people.splice($index, 1);
     }
 }
