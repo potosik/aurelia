66251e50d7ddedff1cae4869b041d3921196e370
Potosik <potosik@gmail.com>
2016-06-21 09:27:04 +0300

    Demo_5 application

---
+++ Aurelia.Tests/Aurelia.Demo_5/src/app.js
@@ -0,0 +1,10 @@
+export class App {
+    configureRouter(config, router) {
+        this.router = router;
+
+        config.map([
+            { route: ["", "people"], moduleId: "people", title: "People", nav: true },
+            { route: "about", moduleId: "about", title: "About", nav: true }
+        ]);
+    }
+}
---
+++ Aurelia.Tests/Aurelia.Demo_5/src/main.js
@@ -0,0 +1,11 @@
+﻿export function configure (aurelia) {
+    aurelia.use.standardConfiguration();
+    //aurelia.use
+    //    .defaultBindingLanguage()
+    //    .defaultResources()
+    //    .history()
+    //    .router()
+    //    .eventAggregator();
+
+    aurelia.start().then(x => x.setRoot());
+}
